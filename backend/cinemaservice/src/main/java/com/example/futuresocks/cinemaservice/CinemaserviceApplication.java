c7b6c41c653511fcb828ba21f395048ad9233697
Colin Bell <colin@dogoodercomics.co.uk>
2018-11-30 09:47:53 +0000

    first commit

---
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/CinemaserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.futuresocks.cinemaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaserviceApplication.class, args);
+	}
+}
---
+++ backend/cinemaservice/src/test/java/com/example/futuresocks/cinemaservice/CinemaserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.futuresocks.cinemaservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinemaserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
