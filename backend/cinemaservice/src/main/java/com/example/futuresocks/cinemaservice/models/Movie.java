5ddc168529b9f07661abb1306926be4d75302490
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-05 16:46:45 +0000

    adds get/setters for id in Movie

--- backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/models/Movie.java
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/models/Movie.java
@@ -42,6 +42,14 @@ public class Movie {
     public Movie() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
