abc9ac02f92a35e615cdbf2f61af3022c76fd7c2
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-05 23:07:23 +0000

    adds dataloader

---
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/components/DataLoader.java
@@ -0,0 +1,59 @@
+package com.example.futuresocks.cinemaservice.components;
+
+import com.example.futuresocks.cinemaservice.models.*;
+import com.example.futuresocks.cinemaservice.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    ScreeningRepository screeningRepository;
+
+    @Autowired
+    SeatRepository seatRepository;
+
+    @Autowired
+    TicketRepository ticketRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args){
+        Movie robocop = new Movie("Robocop", "In a dystopic and crime-ridden Detroit, a terminally wounded cop returns to the force as a powerful cyborg haunted by submerged memories.", "https://m.media-amazon.com/images/M/MV5BZWVlYzU2ZjQtZmNkMi00OTc3LTkwZmYtZDVjNmY4OWFmZGJlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,644,1000_AL_.jpg", 103, 1987);
+        movieRepository.save(robocop);
+
+        Room room = new Room("Screen One");
+        roomRepository.save(room);
+
+        for(int i = 1; i <= 10; i++){
+            Seat seat = new Seat(i, room);
+            seatRepository.save(seat);
+        }
+
+        String[] showTimes = new String[]{"11:00", "13:00"};
+
+        for(String showTime: showTimes){
+            Screening screening = new Screening(robocop, showTime, room);
+            screeningRepository.save(screening);
+        }
+
+        for(int i = 1; i <= 5; i ++){
+            Screening screening = screeningRepository.findById(Long.valueOf(1)).get();
+            Seat seat = seatRepository.findById(Long.valueOf(i)).get();
+            Ticket ticket = new Ticket(screening, seat);
+            ticketRepository.save(ticket);
+        }
+
+    }
+}
