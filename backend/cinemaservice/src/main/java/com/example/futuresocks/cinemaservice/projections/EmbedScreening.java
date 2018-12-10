d71b60cb1bbbba99fbd90cdb242b3930cc00228a
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-10 12:04:12 +0000

    Tweaks embedding so that screening ID is exposed in movies

--- backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/projections/EmbedScreening.java
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/projections/EmbedScreening.java
@@ -12,7 +12,6 @@ import java.util.List;
 public interface EmbedScreening {
     long getId();
     String getTime();
-    Movie getMovie();
     List<Ticket> getTickets();
     Room getRoom();
 }
--- backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/repositories/MovieRepository.java
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/repositories/MovieRepository.java
@@ -5,7 +5,7 @@ import com.example.futuresocks.cinemaservice.projections.EmbedMovie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(excerptProjection = EmbedMovie.class)
+//@RepositoryRestResource(excerptProjection = EmbedMovie.class)
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 }
