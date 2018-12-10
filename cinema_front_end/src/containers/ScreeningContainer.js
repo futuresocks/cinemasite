b5a1d7e9a6c6c32112b0d3733e8447704668f305
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-10 16:03:27 +0000

    adds modal for seatpicker

--- cinema_front_end/src/containers/ScreeningContainer.js
+++ cinema_front_end/src/containers/ScreeningContainer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Modal from 'react-modal';
 import TicketSelector from '../components/TicketSelector';
 import PriceDisplay from '../components/PriceDisplay';
 import prices from '../constants/Prices'
@@ -9,10 +10,21 @@ class ScreeningContainer extends Component {
     this.state = {
       selectedTickets: [],
       selectedSeats: [],
-      ticketPrice: 0
+      ticketPrice: 0,
+      modalIsOpen: false
     }
     this.calculatePrice = this.calculatePrice.bind(this);
     this.handleTicketSubmit = this.handleTicketSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
   }
 
   componentDidMount(){
@@ -37,6 +49,24 @@ class ScreeningContainer extends Component {
   render(){
     return (
       <>
+      <button onClick={this.openModal}>Open Modal</button>
+       <Modal
+         isOpen={this.state.modalIsOpen}
+         onRequestClose={this.closeModal}
+         contentLabel="Example Modal"
+       >
+
+         <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+         <button onClick={this.closeModal}>close</button>
+         <div>I am a modal</div>
+         <form>
+           <input />
+           <button>tab navigation</button>
+           <button>stays</button>
+           <button>inside</button>
+           <button>the modal</button>
+         </form>
+       </Modal>
       <PriceDisplay price = {this.state.ticketPrice}/>
       <TicketSelector handleSubmit = {this.handleTicketSubmit}/>
       </>
