37dd2ceba4b0223eaaa9652a19be56e122bae918
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-10 11:58:07 +0000

    embeds properties for screenings

---
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/projections/EmbedScreening.java
@@ -0,0 +1,18 @@
+package com.example.futuresocks.cinemaservice.projections;
+
+import com.example.futuresocks.cinemaservice.models.Movie;
+import com.example.futuresocks.cinemaservice.models.Room;
+import com.example.futuresocks.cinemaservice.models.Screening;
+import com.example.futuresocks.cinemaservice.models.Ticket;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+@Projection(name = "embedScreening", types = Screening.class)
+public interface EmbedScreening {
+    long getId();
+    String getTime();
+    Movie getMovie();
+    List<Ticket> getTickets();
+    Room getRoom();
+}
--- backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/repositories/ScreeningRepository.java
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/repositories/ScreeningRepository.java
@@ -1,7 +1,10 @@
 package com.example.futuresocks.cinemaservice.repositories;
 
 import com.example.futuresocks.cinemaservice.models.Screening;
+import com.example.futuresocks.cinemaservice.projections.EmbedScreening;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = EmbedScreening.class)
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
 }
