de3e2c27bdffe261bf23d0419533ea8dd79a2bf0
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-10 10:16:22 +0000

    adds cinema container

---
+++ cinema_front_end/src/containers/CinemaContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class CinemaContainer extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+
+  
+}
+
+export default CinemaContainer;
