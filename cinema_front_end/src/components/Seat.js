f68cdfc3d27a75bfd04c3c02e0bd880d051855e4
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-12 09:32:29 +0000

    right that's the seat images definitely fixed

--- cinema_front_end/src/components/Seat.js
+++ cinema_front_end/src/components/Seat.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const Seat = ({selected, number, seatClick})=>{
 
-  //when clicked, sends the number up and changes to booked
-  //if already PREbooked, unclickable - greyed out?
-
   const handleClick = () => {
     seatClick(number)
   }
