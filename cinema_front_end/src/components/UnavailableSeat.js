b5f0721f784bc2deeaf9ef0ba35b6b3490bef364
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-13 16:45:36 +0000

    adds seat number display to unavailable seats

--- cinema_front_end/src/components/UnavailableSeat.js
+++ cinema_front_end/src/components/UnavailableSeat.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const UnavailableSeat = () => (
+const UnavailableSeat = ({number}) => (
+  <>
+  <h3>{number}</h3>
   <img src="/img/unavailable.png"/>
+  </>
 )
 
 export default UnavailableSeat
