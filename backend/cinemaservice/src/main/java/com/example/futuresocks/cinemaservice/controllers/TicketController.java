30ab1f516868b2efed8edb4a3d677095bc2667f8
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-23 18:23:56 +0000

    ticket/new route will create tickets when screening id and seat ids are provided

--- backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/controllers/TicketController.java
+++ backend/cinemaservice/src/main/java/com/example/futuresocks/cinemaservice/controllers/TicketController.java
@@ -1,12 +1,16 @@
 package com.example.futuresocks.cinemaservice.controllers;
 
+import com.example.futuresocks.cinemaservice.models.Screening;
+import com.example.futuresocks.cinemaservice.models.Seat;
+import com.example.futuresocks.cinemaservice.models.Ticket;
 import com.example.futuresocks.cinemaservice.repositories.ScreeningRepository;
 import com.example.futuresocks.cinemaservice.repositories.SeatRepository;
 import com.example.futuresocks.cinemaservice.repositories.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/tickets")
@@ -21,8 +25,15 @@ public class TicketController {
     @Autowired
     SeatRepository seatRepository;
 
-    @PostMapping("/new")
-    public void createTickets(){
-        System.out.println("hello");
+    @RequestMapping(value = "/new", headers = "Accept=application/json", method = RequestMethod.POST)
+    public void createTickets(@RequestBody HashMap<Long, List<Long>> requestData) {
+        Long screeningId = (Long) requestData.keySet().toArray()[0];
+        Screening screening = screeningRepository.findById(screeningId).get();
+        List<Long> seatIDs = requestData.get(screeningId);
+        for(Long seatID : seatIDs){
+            Seat seat = seatRepository.findById(seatID).get();
+            Ticket ticket = new Ticket(screening, seat);
+            ticketRepository.save(ticket);
+        }
     }
 }
