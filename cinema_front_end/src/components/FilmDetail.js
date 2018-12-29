21f1b03d9e2c9990b09e5b9084cc96a581e1a2bd
Colin Bell <colin@dogoodercomics.co.uk>
2018-12-29 12:03:19 +0000

    moment parsing localDateTime in frontend

--- cinema_front_end/src/components/FilmDetail.js
+++ cinema_front_end/src/components/FilmDetail.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 const FilmDetail = ({movie}) => {
 
   //filter screenings by date here?
-  debugger;
-  const screenings = movie._embedded.screenings.map((screening, index) => <Link key = {index} to={"screenings/" + screening.id}>{screening.time}</Link>)
+  const screenings = movie._embedded.screenings
+                    .map((screening, index) => {
+                      return <Link key = {index}
+                                   to={"screenings/" + screening.id}>
+                                   {moment(screening.dateTime).format('HH:mm')}
+                             </Link>
+                    })
+                    .sort((linkA, linkB) => linkB.key - linkA.key)
 
   return (
     <>
